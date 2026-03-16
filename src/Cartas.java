7e6012cacb4facf11211f5b76fdc2c313944f507
navip <navi.pa.07@gmail.com>
2026-03-16 23:01:46 +0100

    1.1

--- src/Cartas.java
+++ src/Cartas.java
@@ -1,11 +1,13 @@
 public class Cartas {
     String carta1, carta2;
 
-    //Constructor
+    //Constructor PreFlop
     public Cartas(String carta1, String carta2){
         this.carta1 = carta1;
         this.carta2 = carta2;
     }
+
+
 
 
 
