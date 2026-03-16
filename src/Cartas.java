05a1d48d27cb54d8409e39bd9c39a6dfd7930af3
navip <navi.pa.07@gmail.com>
2026-03-16 23:25:31 +0100

    1.2

--- src/Cartas.java
+++ src/Cartas.java
@@ -1,14 +1,48 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Cartas {
-    String carta1, carta2;
+    String preflop1, preflop2;
+    String palo;
+    String valor;
 
-    //Constructor PreFlop
-    public Cartas(String carta1, String carta2){
-        this.carta1 = carta1;
-        this.carta2 = carta2;
+    Scanner sc = new Scanner(System.in);
+
+
+    String[] todasCartas = {
+            // Corazones (C)
+            "AC","2C","3C","4C","5C","6C","7C","8C","9C","JC","QC","KC","10C",
+            // Tréboles (T)
+            "AT","2T","3T","4T","5T","6T","7T","8T","9T","JT","QT","KT","10T",
+            // Picas (P)
+            "AP","2P","3P","4P","5P","6P","7P","8P","9P","JP","QP","KP","10P",
+            // Rombos (R)
+            "AR","2R","3R","4R","5R","6R","7R","8R","9R","JR","QR","KR","10R"
+    };
+
+    public void PreFlopTomarDatosYValido(){
+        System.out.println("Introduce tu 1º carta (ej: AC o KP): ");
+        preflop1 = sc.nextLine();
+        preflop1 = preflop1.toUpperCase();
+        while(!Arrays.asList(todasCartas).contains(preflop1)){
+            System.out.println("Ha habido un fallo en la escritura de la carta 1º");
+            System.out.println("Introduce tu 1º carta (ej: AC o KP): ");
+            preflop1 = sc.nextLine();
+            preflop1 = preflop1.toUpperCase();
+        }
+
+        System.out.println("Introduce tu 2ª carta (ej: AC o KP): ");
+        preflop2 = sc.nextLine();
+        preflop2 = preflop2.toUpperCase();
+        while(!Arrays.asList(todasCartas).contains(preflop2)){
+            System.out.println("Ha habido un fallo en la escritura de la carta 2ª");
+            System.out.println("Introduce tu 2ª carta (ej: AC o KP): ");
+            preflop2 = sc.nextLine();
+            preflop2 = preflop2.toUpperCase();
+
+        }
+
     }
-
-
-
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,16 @@ void main() {
     String preflop2;
 
     Scanner sc = new Scanner(System.in);
+    Cartas cartas = new Cartas();
 
     System.out.println("Los jugadores sin contarte a ti = ");
     jugadores = sc.nextInt();
     sc.nextLine();
 
     System.out.println("Las cartas se deben representar = ");
-    System.out.println("A   2   3   4\n5   6   7   8    + ( ❤=C // ♣=T // ♠=P // \uD83D\uDD36 =R )\n9   J   Q   K");
+    System.out.println("A   2   3   4       ||\n5   6   7   8       ||+ ( ❤=C // ♣=T // ♠=P // \uD83D\uDD36 =R )\n9   10  J   Q   K   ||");
 
-    System.out.println("Introduce tus 2 cartas (ej: AC KP): ");
-    preflop1 = sc.nextLine();
-    preflop2 = sc.nextLine();
+    cartas.PreFlopTomarDatosYValido();
 
 
 
