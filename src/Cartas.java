6e1e2e54ce79e97382db3b604e156d6aabd73237
navip <navi.pa.07@gmail.com>
2026-03-16 22:58:07 +0100

    1.1

---
+++ src/Cartas.java
@@ -0,0 +1,14 @@
+public class Cartas {
+    String carta1, carta2;
+
+    //Constructor
+    public Cartas(String carta1, String carta2){
+        this.carta1 = carta1;
+        this.carta2 = carta2;
+    }
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 
 void main() {
+
+    int jugadores = 0;//solo contrincantes
+    String preflop1;
+    String preflop2;
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Los jugadores sin contarte a ti = ");
+    jugadores = sc.nextInt();
+    sc.nextLine();
+
+    System.out.println("Las cartas se deben representar = ");
+    System.out.println("A   2   3   4\n5   6   7   8    + ( ❤=C // ♣=T // ♠=P // \uD83D\uDD36 =R )\n9   J   Q   K");
+
+    System.out.println("Introduce tus 2 cartas (ej: AC KP): ");
+    preflop1 = sc.nextLine();
+    preflop2 = sc.nextLine();
+
+
+
 
 }
