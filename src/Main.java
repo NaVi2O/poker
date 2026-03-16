c6b4146ee8691cb13a2102768d359ba2b53d3d90
navip <navi.pa.07@gmail.com>
2026-03-16 23:47:26 +0100

    1.3

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ void main() {
 
     cartas.TomarJugadores();
 
-
     System.out.println("Las cartas se deben representar = ");
     System.out.println("A   2   3   4       ||\n5   6   7   8       ||+ ( ❤=C // ♣=T // ♠=P // \uD83D\uDD36 =R )\n9   10  J   Q   K   ||");
 
