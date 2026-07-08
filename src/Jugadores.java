c795ed5ff28259e193eb0da9fa7c9671ab341a29
navip <navi.pa.07@gmail.com>
2026-07-08 03:03:03 +0200

    de0, EMPIEZO

---
+++ src/Jugadores.java
@@ -0,0 +1,12 @@
+public class Jugadores {
+
+    int numJugadores;
+
+    public Jugadores(int numJugadores){
+        this.numJugadores = numJugadores;
+    }
+
+    public void prueba(){
+        System.out.println(numJugadores);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,30 @@ import java.util.Scanner;
 
 void main() {
 
+    int numJugadores;
+    int jugAbandona;
+    String carta1P;
+    String carta2P;
+    String cartaPreFlop1;
+    String cartaPreFlop2;
+    String cartaPreFlop3;
+    String cartaFlop;
+    String cartaTurn;
+
+
+    Scanner sc = new Scanner(System.in);
+
+
+    System.out.println("Cuantos Jugadores hay en tu mesa: ");
+    numJugadores = sc.nextInt();
+    sc.nextLine();
+    Jugadores jugadores = new Jugadores(numJugadores);
+
+
+
+
+
+
 
 
 
